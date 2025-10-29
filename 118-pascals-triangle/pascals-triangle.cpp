dc1ea51ac67d34b6f278775ef6974633db4146ed
Muditmayank Mishra <muditmayankm@gmail.com>
2025-10-29 16:09:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (63.54%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,10 +4,10 @@ public:
         int n = numRows;
         vector<vector<int>> a(n);
         for(int i=0;i<n;i++){
-            int num=1;
+            int key=1;
             for(int k=0;k<=i;k++){
-                a[i].push_back(num);
-                num=num*(i-k)/(k+1);
+                a[i].push_back(key);
+                key = key*(i-k)/(k+1);
             }
         }
         return a;
